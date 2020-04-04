ac728ab2f17273634d83ceb1674f7149b0b54719
Will Scott <willscott@gmail.com>
2020-04-03 18:34:48 -0700

    collects stats

--- go.mod
+++ go.mod
@@ -7,16 +7,16 @@ require (
 	github.com/dgraph-io/badger/v2 v2.0.2
 	github.com/ipfs/go-datastore v0.4.4
 	github.com/ipfs/go-ipfs-addr v0.0.1
-	github.com/ipfs/go-ipns v0.0.2 // indirect
+	github.com/ipfs/go-ipns v0.0.2
 	github.com/ipfs/go-log v1.0.3
 	github.com/ipfs/go-todocounter v0.0.2 // indirect
 	github.com/libp2p/go-conn-security v0.1.0 // indirect
-	github.com/libp2p/go-libp2p v0.7.3-0.20200330204848-85060e8d229f
-	github.com/libp2p/go-libp2p-core v0.5.0
+	github.com/libp2p/go-libp2p v0.7.4
+	github.com/libp2p/go-libp2p-core v0.5.1
 	github.com/libp2p/go-libp2p-host v0.1.0
 	github.com/libp2p/go-libp2p-interface-connmgr v0.1.0 // indirect
 	github.com/libp2p/go-libp2p-interface-pnet v0.1.0 // indirect
-	github.com/libp2p/go-libp2p-kad-dht v0.5.2-0.20200403162821-1506cc0533d2
+	github.com/libp2p/go-libp2p-kad-dht v0.5.3-0.20200403205829-7de8ab4a21fe
 	github.com/libp2p/go-libp2p-metrics v0.1.0 // indirect
 	github.com/libp2p/go-libp2p-net v0.1.0 // indirect
 	github.com/libp2p/go-libp2p-peer v0.2.0
@@ -27,6 +27,8 @@ require (
 	github.com/libp2p/go-libp2p-transport v0.1.0 // indirect
 	github.com/multiformats/go-multiaddr v0.2.1
 	github.com/multiformats/go-multihash v0.0.13
+	github.com/nranchev/go-libGeoIP v0.0.0-20170629073846-d6d4a9a4c7e8
+	github.com/oschwald/maxminddb-golang v1.6.0 // indirect
 	github.com/prometheus/client_golang v1.4.1
 	github.com/spf13/pflag v1.0.3
 	github.com/syndtr/goleveldb v1.0.0
--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"context"
 	"crypto/rand"
 	"fmt"
-	"net/http"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
+	libgeo "github.com/nranchev/go-libGeoIP"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/push"
 
 	ds "github.com/ipfs/go-datastore"
 	ipfsaddr "github.com/ipfs/go-ipfs-addr"
@@ -18,31 +19,29 @@ import (
 	logging "github.com/ipfs/go-log"
 	libp2p "github.com/libp2p/go-libp2p"
 	host "github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/network"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	pstore "github.com/libp2p/go-libp2p-peerstore"
 	record "github.com/libp2p/go-libp2p-record"
 	ma "github.com/multiformats/go-multiaddr"
+	manet "github.com/multiformats/go-multiaddr-net"
 	mh "github.com/multiformats/go-multihash"
 )
-
-const CRAWL_PERIOD = time.Second * 15
 
 var (
 	node_counts_g = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name:      "nodes_total",
 		Subsystem: "stats",
 		Namespace: "libp2p",
-		Help:      "total number of nodes seen in a given time period",
-	}, []string{"interval", "version"})
+		Help:      "total number of dht nodes encountered",
+	}, []string{"version", "cc"})
 
-	protocols_g = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name:      "protocols",
+	node_connectable_g = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name:      "nodes_connectable",
 		Subsystem: "stats",
 		Namespace: "libp2p",
-		Help:      "protocol counts by name",
-	}, []string{"interval", "protocol"})
+		Help:      "connectable dht nodes encountered",
+	}, []string{"version", "cc"})
 
 	query_lat_h = prometheus.NewHistogram(prometheus.HistogramOpts{
 		Name:      "query",
@@ -52,34 +51,28 @@ var (
 		Buckets:   []float64{0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 1, 2, 5, 10, 15, 20, 25, 30, 60},
 	})
 
+	connect_lat_h = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:      "connect",
+		Subsystem: "stats",
+		Namespace: "libp2p",
+		Help:      "h.Connect latencies",
+		Buckets:   []float64{0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 1, 2, 5, 10, 15, 20, 25, 30, 60},
+	})
+
 	addr_counts_s = prometheus.NewSummary(prometheus.SummaryOpts{
 		Name:      "addr_counts",
 		Subsystem: "stats",
 		Namespace: "libp2p",
 		Help:      "address counts discovered by the dht crawls",
 	})
-
-	query_lifetime_h = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:      "lifetime",
-		Subsystem: "dht",
-		Namespace: "libp2p",
-		Help:      "how long dht servers remain online",
-		Buckets:   []float64{1, 5, 15, 60, 60 * 5, 60 * 15, 3600, 3600 * 2, 3600 * 4, 3600 * 12, 3600 * 24, 3600 * 48, 3600 * 72, 3600 * 24 * 7, 3600 * 24 * 14, 3600 * 24 * 30},
-	})
-
-	query_offtime_h = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:      "offtime",
-		Subsystem: "dht",
-		Namespace: "libp2p",
-		Help:      "how long servers stay offline before coming back",
-		Buckets:   []float64{1, 5, 15, 60, 60 * 5, 60 * 15, 3600, 3600 * 2, 3600 * 4, 3600 * 12, 3600 * 24, 3600 * 48, 3600 * 72, 3600 * 24 * 7, 3600 * 24 * 14, 3600 * 24 * 30},
-	})
 )
 
 func init() {
 	prometheus.MustRegister(node_counts_g)
-	prometheus.MustRegister(protocols_g)
+	prometheus.MustRegister(node_connectable_g)
 	prometheus.MustRegister(query_lat_h)
+	prometheus.MustRegister(connect_lat_h)
+	prometheus.MustRegister(addr_counts_s)
 }
 
 var log = logging.Logger("dht_scrape")
@@ -108,37 +101,38 @@ func init() {
 	}
 }
 
-func handlePromWithAuth(w http.ResponseWriter, r *http.Request) {
-	u, p, ok := r.BasicAuth()
-	if !ok {
-		w.WriteHeader(403)
-		return
-	}
-
-	if !(u == "protocol" && p == os.Getenv("IPFS_METRICS_PASSWORD")) {
-		w.WriteHeader(403)
-		return
-	}
-
-	promhttp.Handler().ServeHTTP(w, r)
-}
-
 func main() {
-	http.HandleFunc("/metrics", handlePromWithAuth)
-	go func() {
-		if err := http.ListenAndServe(":1234", nil); err != nil {
-			panic(err)
-		}
-	}()
+	geoDefault := "/usr/share/GeoIP/"
+	if os.Getenv("GEO_PATH") != "" {
+		geoDefault = os.Getenv("GEO_PATH")
+	}
+	geo4, err := libgeo.Load(geoDefault + "GeoIP.dat")
+	if err != nil {
+		log.Error("country detection unvailable. set GEO_PATH to configure.")
+	}
 
-	for {
-		if err := buildHostAndScrapePeers(); err != nil {
-			log.Error("scrape failed: ", err)
+	if err := buildHostAndScrapePeers(geo4); err != nil {
+		log.Error("scrape failed: ", err)
+	}
+
+	if os.Getenv("PUSHGATEWAY_BASIC_AUTH") != "" {
+		user := strings.SplitN(os.Getenv("PUSHGATEWAY_BASIC_AUTH"), ":", 2)
+		push.New(os.Getenv("PUSHGATEWAY"), "content_routing_live").Gatherer(prometheus.DefaultGatherer).BasicAuth(user[0], user[1]).Push()
+	} else {
+		metrics, _ := prometheus.DefaultGatherer.Gather()
+		for _, m := range metrics {
+			fmt.Printf("%s: %s\n", m.Name, m.String())
 		}
 	}
 }
 
-func buildHostAndScrapePeers() error {
+type trackingInfo struct {
+	Addresses    []ma.Multiaddr `json:"a"`
+	AgentVersion string         `json:"av"`
+	Connected    bool           `json:"c"`
+}
+
+func buildHostAndScrapePeers(lg *libgeo.GeoIP) error {
 	fmt.Println("building new node to collect metrics with")
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -152,10 +146,14 @@ func buildHostAndScrapePeers() error {
 		h.Close()
 	}()
 
+	bootstrap(ctx, h)
+	fmt.Printf("connected bootstrap:%v\n", h.Network().Peers())
+
 	mds := ds.NewMapDatastore()
 	mdht, err := dht.New(ctx, h,
 		dht.Datastore(mds),
 		dht.Mode(dht.ModeClient),
+		dht.RoutingTableRefreshPeriod(time.Minute),
 		dht.Validator(record.NamespacedValidator{
 			"pk":   record.PublicKeyValidator{},
 			"ipns": ipns.Validator{KeyBook: h.Peerstore()}}))
@@ -163,21 +161,26 @@ func buildHostAndScrapePeers() error {
 		panic(err)
 	}
 
-	bootstrap(ctx, h)
-
 	knownPeers := make(map[peer.ID]*trackingInfo)
 	seen := 0
 	newSeen := 0
 	kpl := sync.Mutex{}
-
 	ectx, evch := dht.RegisterForLookupEvents(ctx)
 	go func() {
 		for {
 			select {
 			case <-ectx.Done():
 				return
-			case le := <-evch:
+			case le, ok := <-evch:
+				if !ok {
+					return
+				}
 				kpl.Lock()
+				if le.Response == nil || le.Response.Heard == nil {
+					kpl.Unlock()
+					continue
+				}
+				fmt.Printf(",")
 				for _, heardPeer := range le.Response.Heard {
 					seen++
 					t, ok := knownPeers[heardPeer.Peer]
@@ -186,7 +189,6 @@ func buildHostAndScrapePeers() error {
 						t = new(trackingInfo)
 						knownPeers[heardPeer.Peer] = t
 					}
-					t.LastSeen = time.Now()
 				}
 				kpl.Unlock()
 			}
@@ -196,10 +198,14 @@ func buildHostAndScrapePeers() error {
 	prevSeen := 0
 	prevNew := 0
 	for {
-		pokeDHT(ctx, mdht)
+		pokeDHT(ectx, mdht)
+		fmt.Printf("\nconnected to %d peers. in last iteration, %d new peers seen of %d heard of.\n",
+			len(h.Network().Peers()),
+			newSeen-prevNew,
+			seen-prevSeen)
 		kpl.Lock()
-		// go until a process of poking 1000 peers results in finding less than 50 new ones.
-		if seen-prevSeen > 1000 {
+		// go until a process of poking 1000 peers results in finding less than 100 new ones.
+		if seen-prevSeen > 100 {
 			fmt.Printf("new rate is %f\n", float64(newSeen-prevNew)/float64(seen-prevSeen))
 			if float64(newSeen-prevNew)/float64(seen-prevSeen) < 0.05 {
 				kpl.Unlock()
@@ -209,10 +215,10 @@ func buildHostAndScrapePeers() error {
 			prevNew = newSeen
 		}
 		kpl.Unlock()
-		time.Sleep(CRAWL_PERIOD)
 	}
-	fmt.Printf("through the DHT, learned about %d peers\n", seen)
-	if err := scrapePeers(h, mdht, knownPeers); err != nil {
+
+	fmt.Printf("through the DHT, learned about %d distinct peers\n", newSeen)
+	if err := scrapePeers(h, mdht, knownPeers, lg); err != nil {
 		return err
 	}
 	return nil
@@ -236,16 +242,6 @@ func getRandomString() string {
 		panic(err)
 	}
 	return string(o)
-}
-
-type trackingInfo struct {
-	Addresses     []string  `json:"a"`
-	LastConnected time.Time `json:"lc"`
-	LastSeen      time.Time `json:"ls"`
-	FirstSeen     time.Time `json:"fs"`
-	Sightings     int       `json:"n"`
-	AgentVersion  string    `json:"av"`
-	Protocols     []string  `json:"ps"`
 }
 
 // bootstrap (TODO: choose from larger group of peers)
@@ -282,7 +278,7 @@ func scrapeRound(ctx context.Context, wg *sync.WaitGroup, mdht *dht.IpfsDHT, k s
 	start := time.Now()
 	peers, err := mdht.GetClosestPeers(mctx, k)
 	if err != nil {
-		log.Error(err)
+		log.Debug(err)
 		return
 	}
 
@@ -301,7 +297,7 @@ func scrapeRound(ctx context.Context, wg *sync.WaitGroup, mdht *dht.IpfsDHT, k s
 	query_lat_h.Observe(took)
 }
 
-func peerFinder(ctx context.Context, dht *dht.IpfsDHT, ids chan peer.ID, infos chan peer.AddrInfo, wg *sync.WaitGroup) {
+func peerFinder(ctx context.Context, dht *dht.IpfsDHT, ids chan discoverMsg, infos chan discoverMsg, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for {
 		select {
@@ -311,15 +307,21 @@ func peerFinder(ctx context.Context, dht *dht.IpfsDHT, ids chan peer.ID, infos c
 			if !ok {
 				return
 			}
-			info, err := dht.FindPeer(ctx, p)
-			if err != nil {
-				infos <- info
+			cctx, cncl := context.WithTimeout(ctx, time.Second*30)
+			start := time.Now()
+			info, err := dht.FindPeer(cctx, p.ID)
+			took := time.Since(start).Seconds()
+			connect_lat_h.Observe(took)
+			cncl()
+			if err == nil {
+				p.Addresses = info.Addrs
+				infos <- p
 			}
 		}
 	}
 }
 
-func connector(ctx context.Context, h host.Host, infos chan peer.AddrInfo, versions chan string, wg *sync.WaitGroup) {
+func connector(ctx context.Context, h host.Host, infos chan discoverMsg, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for {
 		select {
@@ -329,67 +331,91 @@ func connector(ctx context.Context, h host.Host, infos chan peer.AddrInfo, versi
 			if !ok {
 				return
 			}
-			err := h.Connect(ctx, i)
-			if err != nil {
+			if len(i.Addresses) == 0 {
 				continue
 			}
-			if h.Network().Connectedness(i.ID) == network.Connected {
-				av, _ := h.Peerstore().Get(i.ID, "AgentVersion")
-				versions <- fmt.Sprint(av)
+			cctx, cncl := context.WithTimeout(ctx, time.Second*30)
+			err := h.Connect(cctx, peer.AddrInfo{ID: i.ID, Addrs: i.Addresses})
+			if err != nil {
+				cncl()
+				continue
 			}
+			i.Connected = true
+			av, _ := h.Peerstore().Get(i.ID, "AgentVersion")
+			cncl()
+			i.AgentVersion = fmt.Sprint(av)
 		}
 	}
 }
 
+type discoverMsg struct {
+	peer.ID
+	*trackingInfo
+}
+
 // peerID -> dht.FindPeer -> network.Connect
-func scrapePeers(h host.Host, dht *dht.IpfsDHT, peers map[peer.ID]*trackingInfo) error {
+func scrapePeers(h host.Host, dht *dht.IpfsDHT, peers map[peer.ID]*trackingInfo, lg *libgeo.GeoIP) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	pc := make(chan peer.ID, 30)
+	pc := make(chan discoverMsg, 30)
 	pcw := sync.WaitGroup{}
-	ic := make(chan peer.AddrInfo, 30)
+	ic := make(chan discoverMsg, 30)
 	icw := sync.WaitGroup{}
-	vc := make(chan string, 30)
-	pcw.Add(15)
-	icw.Add(15)
-	for i := 0; i < 15; i++ {
+
+	pcw.Add(200)
+	icw.Add(100)
+	for i := 0; i < 100; i++ {
 		go peerFinder(ctx, dht, pc, ic, &pcw)
-		go connector(ctx, h, ic, vc, &icw)
+		go peerFinder(ctx, dht, pc, ic, &pcw)
+		go connector(ctx, h, ic, &icw)
+		time.Sleep(time.Millisecond * 100)
 	}
 
-	vmap := make(map[string]int)
-	go func() {
-		for {
-			select {
-			case v, ok := <-vc:
-				if !ok {
-					return
-				}
-				vmap[v]++
-			}
-		}
-	}()
-
-	pl := len(peers)
+	i := 0
 	for p := range peers {
-		pc <- p
+		i++
+		pc <- discoverMsg{p, peers[p]}
+		if i%100 == 0 {
+			fmt.Printf(".")
+		}
 	}
 
 	close(pc)
 	pcw.Wait()
 	close(ic)
 	icw.Wait()
-	close(vc)
 
-	cl := 0
-	for v, c := range vmap {
-		cl += c
-		if c > 100 {
-			fmt.Printf("%-15s\t%d\n", v, c)
+	conn := 0
+	for _, c := range peers {
+		node_counts_g.WithLabelValues(c.AgentVersion, countryOf(lg, c.Addresses)).Inc()
+		if c.Connected {
+			conn++
+			node_connectable_g.WithLabelValues(c.AgentVersion, countryOf(lg, c.Addresses)).Inc()
+		}
+		if len(c.Addresses) > 0 {
+			addr_counts_s.Observe(float64(len(c.Addresses)))
 		}
 	}
-	fmt.Printf("%d of %d nodes connected.\n", pl, cl)
+	fmt.Printf("%d of %d nodes connected.\n", conn, len(peers))
 
 	return nil
+}
+
+func countryOf(lg *libgeo.GeoIP, addrs []ma.Multiaddr) string {
+	country := "ZZ"
+	for _, a := range addrs {
+		if manet.IsPublicAddr(a) {
+			ip, _ := manet.ToIP(a)
+			if ip.To4().Equal(ip) {
+				loc := lg.GetLocationByIP(ip.String())
+				country = loc.CountryCode
+			} else {
+				country = "V6"
+			}
+			break
+		}
+	}
+
+	return country
 }
